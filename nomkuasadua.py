4f05c4d325db2f54e7559626cae7cd6a29c64f60
mariahmohddaud <133840288+mariahmohddaud@users.noreply.github.com>
2024-07-03 11:20:13 +0800

    Update nomkuasadua.py

--- nomkuasadua.py
+++ nomkuasadua.py
@@ -1,7 +1,7 @@
 def kuasadua(x):
     return x*x
 
-nom = int (input ("masukkan satu nombor :")
+nom = int (input ("masukkan satu nombor :"))
 
 nom_kuasa = kuasadua(nom)
 print("kuasa dua bagi " ,nom, "ialah", nom_kuasa)
